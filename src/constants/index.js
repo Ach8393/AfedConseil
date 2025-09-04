3c65bcce54f8f954c69abfa1a222e47b54c4ca76
Ach8393 <achrafsadiq@gmail.com>
2025-09-04 16:20:18 +0100

    another one

--- src/constants/index.js
+++ src/constants/index.js
@@ -42,53 +42,53 @@ const counterItems = [
 
 const logoIconsList = [
   {
-    imgPath: "/images/logos/company-logo-1.png",
+    imgPath: "AfedConseil/images/logos/company-logo-1.png",
   },
   {
-    imgPath: "/images/logos/company-logo-2.png",
+    imgPath: "AfedConseil/images/logos/company-logo-2.png",
   },
   {
-    imgPath: "/images/logos/company-logo-3.png",
+    imgPath: "AfedConseil/images/logos/company-logo-3.png",
   },
   {
-    imgPath: "/images/logos/company-logo-4.png",
+    imgPath: "AfedConseil/images/logos/company-logo-4.png",
   },
   {
-    imgPath: "/images/logos/company-logo-5.png",
+    imgPath: "AfedConseil/images/logos/company-logo-5.png",
   },
   {
-    imgPath: "/images/logos/company-logo-6.png",
+    imgPath: "AfedConseil/images/logos/company-logo-6.png",
   },
   {
-    imgPath: "/images/logos/company-logo-7.png",
+    imgPath: "AfedConseil/images/logos/company-logo-7.png",
   },
   {
-    imgPath: "/images/logos/company-logo-8.png",
+    imgPath: "AfedConseil/images/logos/company-logo-8.png",
   },
   {
-    imgPath: "/images/logos/company-logo-9.png",
+    imgPath: "AfedConseil/images/logos/company-logo-9.png",
   },
   {
-    imgPath: "/images/logos/company-logo-10.png",
+    imgPath: "AfedConseil/images/logos/company-logo-10.png",
   },
   {
-    imgPath: "/images/logos/company-logo-11.png",
+    imgPath: "AfedConseil/images/logos/company-logo-11.png",
   },
 ];
 
 const abilities = [
   {
-    imgPath: "/images/seo.png",
+    imgPath: "AfedConseil/images/seo.png",
     title: "Focus sur la qualité",
     desc: "Fournir des résultats de haute qualité tout en accordant une attention particulière à chaque détail.",
   },
   {
-    imgPath: "/images/chat.png",
+    imgPath: "AfedConseil/images/chat.png",
     title: "Communication fiable",
     desc: "Vous tenir informé à chaque étape afin de garantir transparence et clarté.",
   },
   {
-    imgPath: "/images/time.png",
+    imgPath: "AfedConseil/images/time.png",
     title: "Livraison dans les délais",
     desc: "Veiller à ce que les projets soient réalisés dans les délais, avec qualité et souci du détail.",
   },
@@ -153,8 +153,8 @@ const techStackIcons = [
 const expCards = [
   {
     review: "Porté par Act4Community Khouribga | Piloté par AFED Conseil dans le cadre de notre mission de former une nouvelle génération d’éducateurs sportifs compétents, engagés et pédagogues.",
-    imgPath: "/images/exp1.png",
-    logoPath: "/images/logo1.png",
+    imgPath: "AfedConseil/images/exp1.png",
+    logoPath: "AfedConseil/images/logo1.png",
     title: "Qualification des Entraîneurs d’Éducation Physique et Sportive ",
     date: "Juillet 2025 ",
     responsibilities: [
@@ -164,8 +164,8 @@ const expCards = [
   },
   {
     review: "la 2e session de formation en planification stratégique – Vers une feuille de route structurée et durable pour l’Association Chouala",
-    imgPath: "/images/exp2.png",
-    logoPath: "/images/logo2.png",
+    imgPath: "AfedConseil/images/exp2.png",
+    logoPath: "AfedConseil/images/logo2.png",
     title: "Planification strategique",
     date: "Juin 2025",
     responsibilities: [
@@ -192,15 +192,15 @@ const expCards = [
 const expLogos = [
   {
     name: "logo1",
-    imgPath: "/images/logo1.png",
+    imgPath: "AfedConseil/images/logo1.png",
   },
   {
     name: "logo2",
-    imgPath: "/images/logo2.png",
+    imgPath: "AfedConseil/images/logo2.png",
   },
   {
     name: "logo3",
-    imgPath: "/images/logo3.png",
+    imgPath: "AfedConseil/images/logo3.png",
   },
 ];
 
@@ -210,61 +210,61 @@ const testimonials = [
     mentions: "@estherhoward",
     review:
       "I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.",
-    imgPath: "/images/client1.png",
+    imgPath: "AfedConseil/images/client1.png",
   },
   {
     name: "Wade Warren",
     mentions: "@wadewarren",
     review:
       "Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.",
-    imgPath: "/images/client3.png",
+    imgPath: "AfedConseil/images/client3.png",
   },
   {
     name: "Guy Hawkins",
     mentions: "@guyhawkins",
     review:
       "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    imgPath: "/images/client2.png",
+    imgPath: "AfedConseil/images/client2.png",
   },
   {
     name: "Marvin McKinney",
     mentions: "@marvinmckinney",
     review:
       "Adrian was a pleasure to work with. He turned our outdated website into a fresh, intuitive platform that’s both modern and easy to navigate. Fantastic work overall.",
-    imgPath: "/images/client5.png",
+    imgPath: "AfedConseil/images/client5.png",
   },
   {
     name: "Floyd Miles",
     mentions: "@floydmiles",
     review:
       "Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional!",
-    imgPath: "/images/client4.png",
+    imgPath: "AfedConseil/images/client4.png",
   },
   {
     name: "Albert Flores",
     mentions: "@albertflores",
     review:
       "Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend and backend dev are top-notch.",
-    imgPath: "/images/client6.png",
+    imgPath: "AfedConseil/images/client6.png",
   },
 ];
 
 const socialImgs = [
   {
     name: "insta",
-    imgPath: "/images/insta.png",
+    imgPath: "AfedConseil/images/insta.png",
   },
   {
     name: "fb",
-    imgPath: "/images/fb.png",
+    imgPath: "AfedConseil/images/fb.png",
   },
   {
     name: "x",
-    imgPath: "/images/x.png",
+    imgPath: "AfedConseil/images/x.png",
   },
   {
     name: "linkedin",
-    imgPath: "/images/linkedin.png",
+    imgPath: "AfedConseil/images/linkedin.png",
   },
 ];
 
